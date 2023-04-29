db63cd180bdee4c25ae6f0244a48386f5f99a3d0
aeemkii <limkengha@gmail.com>
2023-04-29 16:04:43 +0600

    second

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,8 +2,25 @@ from django.urls import path
 
 from . import views
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Shop",
+        default_version='v1',
+        description="Shop api",
+        contact=openapi.Contact(email="limkengha@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
+
 urlpatterns = [
+    path("docs/", schema_view.with_ui("swagger")),
     path("products/", views.ProductListAPIView.as_view()), 
     path("categories/", views.CategoryListAPIView.as_view()),
-
+    
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from apps.api.serializer import ProductSerializer, CategorySerializer
 from apps.shop.models import Product, Category
-
+# from apps.accounts.models import UserManager
 class ProductListAPIView(ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.filter(is_available=True)
@@ -13,3 +13,7 @@ class ProductListAPIView(ListAPIView):
 class CategoryListAPIView(ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+# class UserListAPI(ListAPIView):
+#     serializer_class = UserManager
+#     queryset = UserManager.objects.all()
